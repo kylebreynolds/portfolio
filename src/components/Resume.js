fe1f535af69e88750e4a133c8140d62a5f562a8c
Kyle Reynolds <kbreyn06@gmail.com>
2022-09-13 12:31:59 -0500

    added resume download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,30 +1,21 @@
 import React from "react";
 
 function Resume() {
-    // Function will execute on click of button
-    const onButtonClick = () => {
-      // using Java Script method to get PDF file
-      fetch('testpdf.pdf').then(response => {
-          response.blob().then(blob => {
-              // Creating new object of PDF file
-              const fileURL = window.URL.createObjectURL(blob);
-              // Setting various property values
-              let alink = document.createElement('a');
-              alink.href = fileURL;
-              alink.download = 'testpdf.pdf';
-              alink.click();
-          })
-      })
-  }
   return (
-      <>
-          <center>
-              
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"  onClick={onButtonClick}>
-                  RESUME
-              </button>
-          </center>
-      </>
+    <div>
+      <a
+        href="testpdf.pdf"
+        download
+      >
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+  <span>Resume</span>
+</button>
+      </a>
+   
+      
+    </div>
   );
-};
+}
+
 export default Resume;
